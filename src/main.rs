62f24601acae7ec0c008b6e965b6615bf08ae152
Bradley Pizzimenti <bradley@gennakersystems.com>
2023-07-07 16:19:54 -0800

    update user input and output methods

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io; // For user input
+use std::io::Write; // For stdout flush
 use num_bigint::BigUint; // For arbitrarily large integers
 use num_traits::One; // For BigUint::one()
 use rayon::prelude::*; // For parallel iterators
@@ -16,29 +17,49 @@ fn num_digits(n: &BigUint) -> f64 {
 }
 
 fn main() {
-    // Request maximum number from user
-    println!("Please enter the maximum number of digits to compute the factorial for:");
+    // Prompt user for maximum number of decimal places
+    println!("Please enter a number between 1000 and 5000:");
 
-    let mut max_value_string = String::new();
-    io::stdin().read_line(&mut max_value_string).unwrap();
-    let max_value: usize = max_value_string.trim().parse().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
 
-    // Create a parallel iterator over the range from 0 to max_value
-    let input_data: Vec<usize> = (0..=max_value).collect();
+    let max_value: usize = match input.trim().parse() {
+        Ok(num) => {
+            if num >= 1000 && num <= 5000 {
+                num
+            } else {
+                println!("Number out of range. Defaulting to 1000.");
+                1000
+            }
+        },
+        Err(_) => {
+            println!("Invalid input. Defaulting to 5000.");
+            5000
+        },
+    };
 
-    // Process each number in parallel, and reduce the computation times into a total
-    let total_computation_time: Duration = input_data.into_par_iter().map(|i| {
-        let start = Instant::now(); // Start timing
-        let factorial = factorial(i); // Compute the factorial
-        let computation_time = start.elapsed(); // Compute elapsed time
+    let mut work_units_completed: usize = 0;
 
-        let digits = num_digits(&factorial); // Compute number of digits
-        println!("The factorial of {} has approximately {:.2e} digits", i, digits);
-        println!("Computation time: {:?}", computation_time);
+    loop {
+        // Create a parallel iterator over the range from 0 to max_value
+        let input_data: Vec<usize> = (0..=max_value).collect();
 
-        computation_time // Return the computation time
-    }).reduce(|| Duration::new(0, 0), |a, b| a + b); // Reduce computation times into a total
+        // Process each number in parallel, and reduce the computation times into a total
+        let total_computation_time: Duration = input_data.into_par_iter().map(|i| {
+            let start = Instant::now(); // Start timing
+            let factorial = factorial(i); // Compute the factorial
+            let computation_time = start.elapsed(); // Compute elapsed time
 
-    // Print total computation time
-    println!("Total computation time: {:?}", total_computation_time);
+            let _digits = num_digits(&factorial); // Compute number of digits
+
+            computation_time // Return the computation time
+        }).reduce(|| Duration::new(0, 0), |a, b| a + b); // Reduce computation times into a total
+
+        // Increment completed work units counter
+        work_units_completed += 1;
+
+        // Clear the line and print message
+        print!("\x1B[2K\r{} factorials computed to {} decimal places in {:.2} seconds", work_units_completed, max_value, total_computation_time.as_secs_f64());
+        std::io::stdout().flush().unwrap();
+    }
 }
